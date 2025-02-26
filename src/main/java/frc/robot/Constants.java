50aa9799482d4595cd758835c596d46f2e12982b
Robby McMurry <rrmcmurry@gmail.com>
2025-02-26 13:23:05 -0500

    Update Constants.java
    Updating constants to make the CAN IDs make sense.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,15 +50,15 @@ public final class Constants {
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 6;
-    public static final int kRearLeftDrivingCanId = 3;
+    public static final int kFrontLeftDrivingCanId = 1;
     public static final int kFrontRightDrivingCanId = 2;
-    public static final int kRearRightDrivingCanId = 8;
+    public static final int kRearRightDrivingCanId = 3;
+    public static final int kRearLeftDrivingCanId = 4;
 
-    public static final int kFrontLeftTurningCanId = 7;
-    public static final int kRearLeftTurningCanId = 4;
-    public static final int kFrontRightTurningCanId = 9;
-    public static final int kRearRightTurningCanId = 5;
+    public static final int kFrontLeftTurningCanId = 5;    
+    public static final int kFrontRightTurningCanId = 6;
+    public static final int kRearRightTurningCanId = 7;
+    public static final int kRearLeftTurningCanId = 8;
     
     public static final double kUnitstoFeet = 4.2;
   }
